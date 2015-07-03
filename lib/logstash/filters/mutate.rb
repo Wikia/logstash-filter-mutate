e0bbb8d2258f3dd56f6f5a0b83a3da238a6ac567
artursitarski <artur.sitarski@wikia-inc.com>
2015-07-03 13:58:18 +0000

    Wikia changes

--- lib/logstash/filters/mutate.rb
+++ lib/logstash/filters/mutate.rb
@@ -342,7 +342,7 @@ class LogStash::Filters::Mutate < LogStash::Filters::Base
   def uppercase(event)
     @uppercase.each do |field|
       if event[field].is_a?(Array)
-        event[field].each { |v| v.upcase! }
+        event[field].each { |v| v.upcase! if v.is_a?(String) }
       elsif event[field].is_a?(String)
         event[field].upcase!
       else
@@ -356,7 +356,15 @@ class LogStash::Filters::Mutate < LogStash::Filters::Base
   def lowercase(event)
     @lowercase.each do |field|
       if event[field].is_a?(Array)
-        event[field].each { |v| v.downcase! }
+        event[field].each { |v| 
+          if v.is_a?(Array)
+            @logger.debug("Can't downcase something that isn't a string #{v}")
+          elsif v.is_a?(Hash)
+            @logger.debug("Can't downcase something that isn't a string #{v}")
+          else
+            v.downcase!
+          end
+        }
       elsif event[field].is_a?(String)
         event[field].downcase!
       else
